7aa9be1a748fa6f73947660534cc1802492c46e8
Thielson Almendra <thielson12@gmail.com>
2026-03-10 12:39:32 -0300

    feat: add capabilities structure to mcpServer initialization

--- src/mcp-server.ts
+++ src/mcp-server.ts
@@ -3,6 +3,11 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 const mcpServer = new McpServer({
 	name: "Database Table Details",
 	version: "1.0.0",
+	capabilities: {
+		tools: {},
+		prompts: {},
+		resources: {},
+	},
 });
 
 export { mcpServer };
